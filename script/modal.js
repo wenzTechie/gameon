15c637cc638f97d9904549d246f65842e49437ef
wenzTechie <wenzelster@gmail.com>
2021-08-19 11:07:40 -0500

    Fixed double scroll

--- script/modal.js
+++ script/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const html = document.getElementsByTagName("html")[0];
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +20,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  html.classList.add("stop-scroll");
 }
 
 function validate(){
